9bcaf408094e159a411e4580aeb57493f52d9ac0
Minwei Xu <faceswilliam@gmail.com>
2016-06-18 19:44:42 +0800

    301

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ var path = require('path');
 
 const port = process.env.PORT || 3000;
 
+//301 Redirect
+app.use(function(req,res,next){
+        var host = req.get('host');
+        if(/^www\./.test(host)){
+            host = host.substring(4, host.length);
+            res.writeHead(301, {'Location':req.protocol + '://' + host + req.originalUrl,
+                'Expires': new Date().toGMTString()});
+            res.end();
+        } else {
+            next();
+        }
+    });
+    
 app.use(compression({ threshold: 0 }));
 app.use(express.static('public'));
 app.use(bodyParser.json());
