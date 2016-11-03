008c7041b6e477868cffc1a8cef9ce8c386b0e9a
Minwei Xu <faceswilliam@gmail.com>
2016-11-03 01:13:50 -0400

    minweixu.com

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -25,7 +25,7 @@ const About = React.createClass({
           </figure>
           <p>While taking part in competitional programming using Pascal, I self-taught Java, C and C++ in my last of high school.In the summer in 2015, I was lucky to have a chance to be part of a research of data visualization in Shanghai Jiao
             Tong University.</p>
-          <p>I used D3.js to visualize the chemical analysis data of <a href="http://wine.mwxu.me">wine</a> and the customer survey data of a <a href="http://hotel.mwxu.me">hotel</a>.
+          <p>I used D3.js to visualize the chemical analysis data of <a href="http://wine.minweixu.com">wine</a> and the customer survey data of a <a href="http://hotel.minweixu.com">hotel</a>.
             <p>This research introduced me web development and development, and I hence studied some basics of JavaScript, HTML/CSS.
             </p>
           </p>
