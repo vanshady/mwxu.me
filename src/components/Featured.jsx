6dff1800a14a17de982f8315d42a70e992fa03d7
Minwei Xu <faceswilliam@gmail.com>
2016-12-19 14:42:19 -0500

    Revert "university"
    This reverts commit ba9bc3ba1c251b3ee5bbd21b032bba887408f79e.

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,6 +1,8 @@
 const React = require('react');
 
-const Projects = () =>
+const Projects = React.createClass({
+    render() {
+        return (
       <section className="featured">
         <div className="textspace top">
           <aside>
@@ -37,13 +39,6 @@ const Projects = () =>
                 <em className="detail">2016</em>
               </a>
             </div>
-            <div style={{ position: 'relative', left: '10px' }}>
-              <a href="/projects/2016-University">
-                <img src="/img/university.png" className="fullimg" />
-                <b className="label">University</b>
-                <em className="detail">2016</em>
-              </a>
-            </div>
             <div>
               <a href="/projects/2016-SiliconHacks">
                 <img src="/img/silicon.png" className="fullimg" />
@@ -51,8 +46,18 @@ const Projects = () =>
                 <em className="detail">2016</em>
               </a>
             </div>
+            <div style={{ position: 'relative', left: '10px' }}>
+              <a href="/projects/2016-Eventbrite Data Visualization">
+                <img src="/img/GMapFull.png" className="fullimg" />
+                <b className="label">Eventbrite Data Visualization</b>
+                <em className="detail">2016</em>
+              </a>
+            </div>
           </div>
         </div>
-      </section>;
+      </section>
+    );
+    },
+});
 
 module.exports = Projects;
