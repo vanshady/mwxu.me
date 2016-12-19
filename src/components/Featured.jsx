ba9bc3ba1c251b3ee5bbd21b032bba887408f79e
Minwei Xu <faceswilliam@gmail.com>
2016-12-19 14:42:19 -0500

    university

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,8 +1,6 @@
 const React = require('react');
 
-const Projects = React.createClass({
-    render() {
-        return (
+const Projects = () =>
       <section className="featured">
         <div className="textspace top">
           <aside>
@@ -39,6 +37,13 @@ const Projects = React.createClass({
                 <em className="detail">2016</em>
               </a>
             </div>
+            <div style={{ position: 'relative', left: '10px' }}>
+              <a href="/projects/2016-University">
+                <img src="/img/university.png" className="fullimg" />
+                <b className="label">University</b>
+                <em className="detail">2016</em>
+              </a>
+            </div>
             <div>
               <a href="/projects/2016-SiliconHacks">
                 <img src="/img/silicon.png" className="fullimg" />
@@ -46,18 +51,8 @@ const Projects = React.createClass({
                 <em className="detail">2016</em>
               </a>
             </div>
-            <div style={{ position: 'relative', left: '10px' }}>
-              <a href="/projects/2016-Eventbrite Data Visualization">
-                <img src="/img/GMapFull.png" className="fullimg" />
-                <b className="label">Eventbrite Data Visualization</b>
-                <em className="detail">2016</em>
-              </a>
-            </div>
           </div>
         </div>
-      </section>
-    );
-    },
-});
+      </section>;
 
 module.exports = Projects;
